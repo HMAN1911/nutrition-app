450fdb74ec8c06aac95e96af68ad3e88acfc6639
Harry Aydin <harry.aydin.23@gmail.com>
2017-08-06 16:50:05 +1000

    Clean up files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-// if you've done work in node,
-// the below would be equivalent to var React = require('react')
 import React from 'react';
-// we can import static assets like images thanks to some
-// preconfiguration coming from create-react-app.
-import logo from './logo.svg';
-// once we import a style sheet, that will be injected into
-// the document.
-import './App.css';
 
 import FoodInput from './FoodInput';
 import FoodList from './FoodList';
+import './App.css';
 
-// classes are new in javascript 6 and beyond, and are not specific
-// to react. They are useful in many other circumstances too!
-// more information: https://ponyfoo.com/articles/es6-classes-in-depth
-
-// this component will be holding the majority of the 'state' of the application.
 class App extends React.Component {
   state = {
     calorieTarget: 0,
@@ -74,11 +62,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {/* 
-            we need to tell javascript that we want the code between the { } 
-            to be executed or interpreted as regular old javascript. Anything
-            not inside the { } will just be processed as plain text.
-         */}
         <div className="App-totalCount">
           <div className="App-totalCount--calorieDisplay">
             {this.calculateTotalCaloriesConsumed()}
@@ -87,12 +70,18 @@ class App extends React.Component {
             Calories Consumed
           </div>
         </div>
-        <FoodList foodItems={this.state.foodItems} onPanelClick={this.handleTogglePanel} panelOpen={this.state.islistPanelOpen} />
-        <FoodInput onAddFood={this.handleAddFood} onInputChange={this.handleFoodInputFormChange} />
+        <FoodList
+          foodItems={this.state.foodItems}
+          onPanelClick={this.handleTogglePanel}
+          panelOpen={this.state.islistPanelOpen}
+        />
+        <FoodInput
+          onAddFood={this.handleAddFood}
+          onInputChange={this.handleFoodInputFormChange}
+        />
       </div>
     );
   }
-
 }
 
 export default App;
